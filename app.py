cc2ecad4f621d9f5890a806036647f162b45fc2a
Md Zahid Quraishi <zahidquraishi41@gmail.com>
2021-12-18 20:39:36 +0530

    code cleanup

--- app.py
+++ app.py
@@ -126,4 +126,4 @@ def error():
 if __name__ == '__main__':
     url = f'http://127.0.0.1:5000/'
     threading.Timer(1, lambda: webbrowser.open_new(url)).start()
-    app.run(debug=True)
+    app.run(debug=False)
