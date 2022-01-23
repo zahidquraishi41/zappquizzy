c94ef733719e96bf08fef4c4116487319a719575
zahid <zahidquraishi41>
2022-01-23 16:53:20 +0530

    reduced collision in uid generation

--- static/js/localdb.js
+++ static/js/localdb.js
@@ -48,7 +48,19 @@ function userScores(val) {
 function getUserId() {
     let userId = localStorage.getItem('user-id')
     if (userId) return userId
-    userId = Date.now()
+    userId = generateUniqueId()
     localStorage.setItem('user-id', userId)
     return userId
+}
+
+function generateUniqueId() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-='
+    const chars_len = chars.length
+    let id = ''
+    for (let i=0; i<8; i++) {
+        let index = Math.trunc((Math.random() * (chars_len - 1)))
+        id += chars[index]
+    }
+    const time = Date.now()
+    return time + id
 }
